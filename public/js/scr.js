393e4c7ee6bbcfaf7b9f7b59f8ee2c01dd753bf0
Dyo <lloydmur4@gmail.com>
2019-09-27 01:14:00 -0400

    Added more text
    Added text for SE section of skills.
    Adjusted BIo.
    Experimented with Date()

--- public/js/scr.js
+++ public/js/scr.js
@@ -1,2 +1,9 @@
-var utc = new Date().toString().slice(0,10);
+var utc = new Date().toString();
 console.log(utc);
+
+var tick = 1;
+setInterval(function(){
+  if(tick)
+  var time = new Date().toString().slice(15,24);
+  console.log(time);
+},1000)
